ce98b40f1b6c7d60a9bb5d160d2189b1c7bf780b
Cindy Wong <cindywongdev@gmail.com>
2023-01-07 14:16:18 -0500

    created peopleLoader function

--- src/loaders.js
+++ src/loaders.js
@@ -1,1 +1,7 @@
 const URL = "https://peoples-api.onrender.com"
+
+export const peopleLoader = async () => {
+    const response = await fetch(URL + "/people")
+    const people = await response.json()
+    return people
+}
